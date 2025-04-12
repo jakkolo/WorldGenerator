2cc99327dbe1380cebcf254441a0c83524be57fd
edit <jakob.hammersen@gmx.de>
2025-04-12 22:58:26 +0200

    Starting everything, adding the basics, such as colors and specific interfaces

---
+++ src/constants/floorTiles.java
@@ -0,0 +1,15 @@
+package constants;
+
+public enum floorTiles {
+    DIRT(97,74,11),
+    GRASS(28, 120, 5),
+    WATER(3, 6, 163);
+
+    public final int[] color = new int[3];
+
+    private floorTiles(int red, int green, int blue){
+        color[0]=red;
+        color[1]=green;
+        color[2]=blue;
+    }
+}
